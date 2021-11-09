a2381d372b738a1c6b8be11d13b325ec5f6fc1ee
DemJu <julian-efimov@ya.ru>
2021-11-09 22:49:14 +0400

    Request option v.2

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,19 +9,21 @@ public class Application {
     public static void main(String[] args) {
         try (Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD)) {
             final Statement statement = connection.createStatement();
-            final ResultSet resultSet = statement.executeQuery("SELECT DISTINCT c.\"name\" as c_name, c.phone, o.\"name\" as o_name, o.price\n" +
+            final ResultSet resultSet = statement.executeQuery("SELECT DISTINCT c.\"name\" as c_name, c.phone, (SELECT sum(o1.price) \n" +
+                    "\t\t\t\t\t\t\t\t\tFROM customer_orders co1\n" +
+                    "\t\t\t\t\t\t\t\t\t\tleft join orders o1 on co1.order_id = o1.id\n" +
+                    "\t\t\t\t\t\t\t\t\twhere co1.customer_id = co.customer_id) as sumPrice\n" +
                     "FROM customer c\n" +
                     "\t inner join customer_orders co on c.id = co.customer_id\n" +
                     "\t left join  orders o on co.order_id = o.id\n" +
-                    "ORDER by c.\"name\", o.price DESC");
+                    "ORDER by sumPrice DESC");
 
             //Вывод делал для красоты)
             System.out.printf("Query result:\n===============================\n");
             while (resultSet.next()) {
                 System.out.println(conclusionName(resultSet.getString("c_name")) +
                         resultSet.getString("phone") + " || " +
-                        conclusionName(resultSet.getString("o_name")) +
-                        resultSet.getString("price"));
+                        resultSet.getString("sumPrice"));
             }
 
         } catch (SQLException e) {
